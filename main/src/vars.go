f0594424f0e83466820f77711ded48184b2ce356
Bora Guven <guvenbe@gmail.com>
2022-05-24 19:09:01 -0400

    vars

--- main/src/vars.go
+++ main/src/vars.go
@@ -1,5 +1,40 @@
 package main
 
-func main() {
+import "fmt"
 
+var (
+	name, location string
+	age            int
+)
+
+// or one by
+
+var name1 string
+var age1 int
+var location1 string
+
+//initialize
+
+var (
+	name3     string = "Prince Oberyn"
+	age3      int    = 32
+	location3 string = "Dorne"
+)
+
+//If an initializer is present, the type can be omitted
+var (
+	name4     = "Prince Oberyn"
+	age4      = 32
+	location4 = "Dorne"
+)
+
+//You can also initialize multiple variables at once.
+var (
+	name5, location5, age5 = "Prince Oberyn", "Dorne", 32
+)
+
+func main() {
+	name, location := "Prince Oberyn", "Dorne"
+	age := 32
+	fmt.Printf("%s age %d from %s ", name, age, location)
 }
