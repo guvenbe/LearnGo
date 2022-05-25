927e1cdca738911b871f2454da28eabbca9d34f3
Bora Guven <guvenbe@gmail.com>
2022-05-24 20:16:29 -0400

    imports

---
+++ imports/imports2.go
@@ -0,0 +1,14 @@
+package main
+
+// run from terminal
+// go install  github.com/mattetti/goRailsYourself/crypto@latest
+// ls $GOPATH
+import (
+	"fmt"
+	"math"
+)
+
+func main() {
+	fmt.Printf("Now you have %g problems.", math.Sqrt(7))
+	fmt.Println(math.Pi)
+}
