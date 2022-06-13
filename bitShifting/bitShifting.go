ba0e2a37dc87dbc075f90a3563a8ff1a5adfc0af
Bora Guven <guvenbe@gmail.com>
2022-06-12 23:49:38 -0400

    bit shifting

--- bitShifting/bitShifting.go
+++ bitShifting/bitShifting.go
@@ -9,6 +9,13 @@ const (
 	gb = 1 << (iota * 10)
 )
 
+const (
+	year1 = 2022 - iota
+	year2 = 2022 - iota
+	year3 = 2022 - iota
+	year4 = 2022 - iota
+)
+
 func main() {
 	fmt.Println("vim-go")
 	x := 4
@@ -22,4 +29,5 @@ func main() {
 	fmt.Printf("%d\t\t\t%b\t\t\t%#X\n", kb, kb, kb)
 	fmt.Printf("%d\t\t\t%b\t\t\t%#X\n", mb, mb, mb)
 	fmt.Printf("%d\t\t%b\t\t%#X\n", gb, gb, gb)
+	fmt.Printf("%d\t%d\t%d\t%d", year1, year2, year3, year4)
 }
