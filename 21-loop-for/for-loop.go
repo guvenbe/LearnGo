4592a9be715aa78352241f39a73dc32ce03752c5
Bora Guven <guvenbe@gmail.com>
2022-06-13 23:16:48 -0400

    x

--- 21-loop-for/for-loop.go
+++ 21-loop-for/for-loop.go
@@ -1,9 +1,38 @@
 package main
 
+import "fmt"
+
 func main() {
+	fmt.Println("starting")
 	x := 1
 	for x < 10 {
-		fmt.println(x)
+		fmt.Println(x)
 		x++
+	}
+
+	fmt.Println("starting1")
+	y := 1
+	for {
+		if y > 9 {
+			break
+
+		}
+		fmt.Println(y)
+		y++
+	}
+	fmt.Println("starting2")
+	z := 1
+	for {
+		z++
+		if z > 100 {
+			break
+		}
+		if z%2 != 0 {
+			continue
+		}
+		fmt.Println(z)
+	}
+	for a := 33; a <= 122; a++ {
+		fmt.Printf("%v\t%#X\t%#U\n", a, a, a)
 	}
 }
